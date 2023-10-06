cec3be9c3eccf486f2803eca92a888c795d3fce8
Ruslan Sydir <rsydir@gmail.com>
2023-10-06 21:23:16 +0200

    Car model add

---
+++ CarTracker/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarTracker.Models
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		public string VIN { get; set; }
+		public string RegistrationNumber { get; set; }
+		public string Brand { get; set; }
+		public string Model { get; set; }
+		public string ProductionYear { get; set; }
+		public string Fuel { get; set; }
+	}
+}
+
