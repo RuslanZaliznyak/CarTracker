d97dc9b70299d0ade47c12227401c907c1d23e8d
Ruslan Sydir <rsydir@gmail.com>
2023-10-06 21:02:54 +0200

    Routes

---
+++ CarTracker/Routes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CarTracker
+{
+	public static class Routes
+	{
+        public static void ConfigureRoutes(IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapGet("/api/car", () =>
+            {
+                Console.WriteLine("api car test");
+            });
+
+            
+        }
+    }
+}
+
